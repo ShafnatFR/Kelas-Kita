b5e0b80aab86cbb614ab5c40577c2eccc75b4786
ShafnatFR <shafnatfuainiramadhan@gmail.com>
2025-05-08 09:10:58 +0700

    Setting notifikasi

--- page/setting-profil.php
+++ page/setting-profil.php
@@ -22,7 +22,6 @@ $user = $stmt->get_result()->fetch_assoc();
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="custom.css" rel="stylesheet">
-    
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"/>
     <title>Profil</title>
 </head>
