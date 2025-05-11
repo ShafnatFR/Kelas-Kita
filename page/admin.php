e8f1663f2ce38e23d8c1349b62f7ec427f2b260b
ShafnatFR <shafnatfuainiramadhan@gmail.com>
2025-05-11 10:39:53 +0700

    Update admin

--- page/admin.php
+++ page/admin.php
@@ -1,4 +1,9 @@
-<html>
+<html lang="id">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <!-- <link href="custom.css" rel="stylesheet"> -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"/>
 
     <head>
         <title>Halaman Admin</title>
