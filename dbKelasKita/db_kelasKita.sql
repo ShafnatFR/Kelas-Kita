02118b4ce2c15256bb8ef9ccb3d559adeff52619
AliqNurShiddiq-TelU <aliqnurshiddiq@student.telkomuniversity.ac.id>
2025-05-24 21:21:10 +0700

    Update db_kelasKita.sql

--- dbKelasKita/db_kelasKita.sql
+++ dbKelasKita/db_kelasKita.sql
@@ -1,8 +1,8 @@
--- phpMyAdmin SQL Dump
--- version 5.2.1
+@ -3,521 +3,79 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
+-- Generation Time: May 22, 2025 at 07:07 AM
 -- Generation Time: May 22, 2025 at 07:15 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.0.30
@@ -20,6 +20,176 @@ SET time_zone = "+00:00";
 --
 -- Database: `kelaskita`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_dokumen`
+--
+
+CREATE TABLE `tb_dokumen` (
+  `id_dokumen` int(30) NOT NULL,
+  `nama_dokumen` varchar(50) NOT NULL,
+  `deskripsi_d` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_kategori`
+--
+
+CREATE TABLE `tb_kategori` (
+  `id_kategori` int(30) NOT NULL,
+  `nama_kategori` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_kategori_kelas`
+--
+
+CREATE TABLE `tb_kategori_kelas` (
+  `id_kategori_kelas` int(30) NOT NULL,
+  `id_kelas` int(30) NOT NULL,
+  `id_kategori` int(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_kelas`
+--
+
+CREATE TABLE `tb_kelas` (
+  `id_kelas` int(30) NOT NULL,
+  `nama_kelas` varchar(255) NOT NULL,
+  `harga` decimal(10,2) NOT NULL,
+  `profil_kelas` varchar(100) DEFAULT NULL,
+  `badge` varchar(100) DEFAULT NULL,
+  `description` text DEFAULT NULL,
+  `id_mentor` int(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_keranjang`
+--
+
+CREATE TABLE `tb_keranjang` (
+  `id_keranjang` int(30) NOT NULL,
+  `tgl_keranjang` date NOT NULL,
+  `id_kelas` int(30) NOT NULL,
+  `id_user` int(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_komentar`
+--
+
+CREATE TABLE `tb_komentar` (
+  `id_komentar` int(30) NOT NULL,
+  `isi` varchar(255) NOT NULL,
+  `id_user` int(30) NOT NULL,
+  `id_kelas` int(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_laporan`
+--
+
+CREATE TABLE `tb_laporan` (
+  `id_report` int(30) NOT NULL,
+  `kategori_report` enum('Penggunaan kata kasar','Matei tidak relevan','Pornografi') NOT NULL,
+  `keterangan_report` varchar(100) NOT NULL,
+  `id_kelas` int(30) NOT NULL,
+  `id_user` int(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_materi`
+--
+
+CREATE TABLE `tb_materi` (
+  `id_materi` int(30) NOT NULL,
+  `deskripsi_m` varchar(255) NOT NULL,
+  `id_kelas` int(30) NOT NULL,
+  `id_sub_materi` int(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_mentor`
+--
+
+CREATE TABLE `tb_mentor` (
+  `id_mentor` int(30) NOT NULL,
+  `status` enum('Aktif','Non-Aktif') NOT NULL,
+  `id_user` int(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_progress_kelas`
+--
+
+CREATE TABLE `tb_progress_kelas` (
+  `id_progress_kelas` int(30) NOT NULL,
+  `id_kelas` int(30) NOT NULL,
+  `id_user` int(30) NOT NULL,
+  `id_materi` int(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_review`
+--
+
+CREATE TABLE `tb_review` (
+  `id_review` int(30) NOT NULL,
+  `bintang_review` enum('1','2','3','4','5') NOT NULL,
+  `isi_review` varchar(100) NOT NULL,
+  `tgl_review` date NOT NULL,
+  `id_user` int(11) NOT NULL,
+  `id_kelas` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_sub_materi`
+--
+
+CREATE TABLE `tb_sub_materi` (
+  `id_sub_materi` int(30) NOT NULL,
+  `deskripsi_sm` varchar(255) NOT NULL,
+  `id_dokumen` int(30) NOT NULL,
+  `id_video` int(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_transaksi`
+--
+
+CREATE TABLE `tb_transaksi` (
+  `id_transaksi` int(30) NOT NULL,
+  `id_keranjang` int(30) NOT NULL,
+  `bukti_transaksi` varchar(50) NOT NULL,
+  `tgl_transaksi` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -51,6 +221,21 @@ CREATE TABLE `tb_user` (
 --
 
 INSERT INTO `tb_user` (`id_user`, `first_name`, `last_name`, `username`, `password`, `role`, `deskripsi`, `fotoProfil`, `balasan_ke_komentar`, `komentar_baru`, `notifikasi_postingan_baru`, `email`, `instagram`, `twitter`, `linkdin`, `github`) VALUES
+(5, '', '', 'Murid', '$2y$10$HKBEA1/qQyxcBoNa3xT4quUsUWHv4hBaNrXP6pAZnvtjzAhyu1BiG', '', NULL, '', 0, 0, 0, '', '', '', '', ''),
+(6, '', '', 'Mentor', '$2y$10$CX9KqmS8jtnx2ncX1A4D/.VBRfjbgAzou8VYdTb.ZnGoUQ3Xn9LBC', '', NULL, '', 0, 0, 0, '', '', '', '', ''),
+(7, '', '', 'User', '$2y$10$Neozo9JGhh6JZb2wR42Rv.A5m6eMZrdos7TubtspxEtfw.MPpydzm', '', NULL, '', 0, 0, 0, '', '', '', '', '');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_video`
+--
+
+CREATE TABLE `tb_video` (
+  `id_video` int(30) NOT NULL,
+  `nama_video` varchar(50) NOT NULL,
+  `deskripsi_v` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 (5, '', '', 'Murid', '$2y$10$HKBEA1/qQyxcBoNa3xT4quUsUWHv4hBaNrXP6pAZnvtjzAhyu1BiG', 'murid', NULL, '', 0, 0, 0, '', '', '', '', ''),
 (6, '', '', 'Mentor', '$2y$10$CX9KqmS8jtnx2ncX1A4D/.VBRfjbgAzou8VYdTb.ZnGoUQ3Xn9LBC', 'mentor', NULL, '', 0, 0, 0, '', '', '', '', ''),
 (7, '', '', 'Admin', '$2y$10$Neozo9JGhh6JZb2wR42Rv.A5m6eMZrdos7TubtspxEtfw.MPpydzm', 'admin', NULL, '', 0, 0, 0, '', '', '', '', '');
@@ -60,20 +245,280 @@ INSERT INTO `tb_user` (`id_user`, `first_name`, `last_name`, `username`, `passwo
 --
 
 --
+-- Indexes for table `tb_dokumen`
+--
+ALTER TABLE `tb_dokumen`
+  ADD PRIMARY KEY (`id_dokumen`);
+
+--
+-- Indexes for table `tb_kategori`
+--
+ALTER TABLE `tb_kategori`
+  ADD PRIMARY KEY (`id_kategori`);
+
+--
+-- Indexes for table `tb_kategori_kelas`
+--
+ALTER TABLE `tb_kategori_kelas`
+  ADD PRIMARY KEY (`id_kategori_kelas`),
+  ADD KEY `fkid_kategori_kk` (`id_kategori`),
+  ADD KEY `fkid_kelas_kk` (`id_kelas`);
+
+--
+-- Indexes for table `tb_kelas`
+--
+ALTER TABLE `tb_kelas`
+  ADD PRIMARY KEY (`id_kelas`),
+  ADD KEY `fkid_mentor_kelas` (`id_mentor`);
+
+--
+-- Indexes for table `tb_keranjang`
+--
+ALTER TABLE `tb_keranjang`
+  ADD PRIMARY KEY (`id_keranjang`),
+  ADD KEY `fkid_user_keranjang` (`id_user`),
+  ADD KEY `fkid_kelas_keranjang` (`id_kelas`);
+
+--
+-- Indexes for table `tb_komentar`
+--
+ALTER TABLE `tb_komentar`
+  ADD PRIMARY KEY (`id_komentar`),
+  ADD KEY `fkid_kelas` (`id_kelas`),
+  ADD KEY `fkid_user` (`id_user`);
+
+--
+-- Indexes for table `tb_laporan`
+--
+ALTER TABLE `tb_laporan`
+  ADD PRIMARY KEY (`id_report`),
+  ADD KEY `fkid_user_laporan` (`id_user`),
+  ADD KEY `fkid_kelas_laporan` (`id_kelas`);
+
+--
+-- Indexes for table `tb_materi`
+--
+ALTER TABLE `tb_materi`
+  ADD PRIMARY KEY (`id_materi`),
+  ADD KEY `fkid_kelas_materi` (`id_kelas`),
+  ADD KEY `fkid_sub_materi_materi` (`id_sub_materi`);
+
+--
+-- Indexes for table `tb_mentor`
+--
+ALTER TABLE `tb_mentor`
+  ADD PRIMARY KEY (`id_mentor`);
+
+--
+-- Indexes for table `tb_progress_kelas`
+--
+ALTER TABLE `tb_progress_kelas`
+  ADD PRIMARY KEY (`id_progress_kelas`),
+  ADD KEY `fkid_user_pk` (`id_user`),
+  ADD KEY `fkid_kelas_pk` (`id_kelas`),
+  ADD KEY `fkid_materi_pk` (`id_materi`);
+
+--
+-- Indexes for table `tb_review`
+--
+ALTER TABLE `tb_review`
+  ADD PRIMARY KEY (`id_review`),
+  ADD KEY `fkid_user_review` (`id_user`),
+  ADD KEY `fkid_kelas_review` (`id_kelas`);
+
+--
+-- Indexes for table `tb_sub_materi`
+--
+ALTER TABLE `tb_sub_materi`
+  ADD PRIMARY KEY (`id_sub_materi`),
+  ADD KEY `fkid_dokumen_sub_materi` (`id_dokumen`),
+  ADD KEY `fkid_video_sub_materi` (`id_video`);
+
+--
+-- Indexes for table `tb_transaksi`
+--
+ALTER TABLE `tb_transaksi`
+  ADD PRIMARY KEY (`id_transaksi`),
+  ADD KEY `fkid_keranjang_transaksi` (`id_keranjang`);
+
+--
 -- Indexes for table `tb_user`
 --
 ALTER TABLE `tb_user`
   ADD PRIMARY KEY (`id_user`);
 
 --
+-- Indexes for table `tb_video`
+--
+ALTER TABLE `tb_video`
+  ADD PRIMARY KEY (`id_video`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
+
+--
+-- AUTO_INCREMENT for table `tb_dokumen`
+--
+ALTER TABLE `tb_dokumen`
+  MODIFY `id_dokumen` int(30) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tb_kategori`
+--
+ALTER TABLE `tb_kategori`
+  MODIFY `id_kategori` int(30) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tb_kategori_kelas`
+--
+ALTER TABLE `tb_kategori_kelas`
+  MODIFY `id_kategori_kelas` int(30) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tb_kelas`
+--
+ALTER TABLE `tb_kelas`
+  MODIFY `id_kelas` int(30) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tb_keranjang`
+--
+ALTER TABLE `tb_keranjang`
+  MODIFY `id_keranjang` int(30) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tb_komentar`
+--
+ALTER TABLE `tb_komentar`
+  MODIFY `id_komentar` int(30) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tb_laporan`
+--
+ALTER TABLE `tb_laporan`
+  MODIFY `id_report` int(30) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tb_materi`
+--
+ALTER TABLE `tb_materi`
+  MODIFY `id_materi` int(30) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tb_mentor`
+--
+ALTER TABLE `tb_mentor`
+  MODIFY `id_mentor` int(30) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tb_progress_kelas`
+--
+ALTER TABLE `tb_progress_kelas`
+  MODIFY `id_progress_kelas` int(30) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tb_review`
+--
+ALTER TABLE `tb_review`
+  MODIFY `id_review` int(30) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tb_sub_materi`
+--
+ALTER TABLE `tb_sub_materi`
+  MODIFY `id_sub_materi` int(30) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tb_transaksi`
+--
+ALTER TABLE `tb_transaksi`
+  MODIFY `id_transaksi` int(30) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `tb_user`
 --
 ALTER TABLE `tb_user`
   MODIFY `id_user` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT for table `tb_video`
+--
+ALTER TABLE `tb_video`
+  MODIFY `id_video` int(30) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `tb_kategori_kelas`
+--
+ALTER TABLE `tb_kategori_kelas`
+  ADD CONSTRAINT `fkid_kategori_kk` FOREIGN KEY (`id_kategori`) REFERENCES `tb_kategori` (`id_kategori`),
+  ADD CONSTRAINT `fkid_kelas_kk` FOREIGN KEY (`id_kelas`) REFERENCES `tb_kelas` (`id_kelas`);
+
+--
+-- Constraints for table `tb_kelas`
+--
+ALTER TABLE `tb_kelas`
+  ADD CONSTRAINT `fkid_mentor_kelas` FOREIGN KEY (`id_mentor`) REFERENCES `tb_mentor` (`id_mentor`);
+
+--
+-- Constraints for table `tb_keranjang`
+--
+ALTER TABLE `tb_keranjang`
+  ADD CONSTRAINT `fkid_kelas_keranjang` FOREIGN KEY (`id_kelas`) REFERENCES `tb_kelas` (`id_kelas`),
+  ADD CONSTRAINT `fkid_user_keranjang` FOREIGN KEY (`id_user`) REFERENCES `tb_user` (`id_user`);
+
+--
+-- Constraints for table `tb_komentar`
+--
+ALTER TABLE `tb_komentar`
+  ADD CONSTRAINT `fkid_kelas` FOREIGN KEY (`id_kelas`) REFERENCES `tb_kelas` (`id_kelas`),
+  ADD CONSTRAINT `fkid_user` FOREIGN KEY (`id_user`) REFERENCES `tb_user` (`id_user`);
+
+--
+-- Constraints for table `tb_laporan`
+--
+ALTER TABLE `tb_laporan`
+  ADD CONSTRAINT `fkid_kelas_laporan` FOREIGN KEY (`id_kelas`) REFERENCES `tb_kelas` (`id_kelas`),
+  ADD CONSTRAINT `fkid_user_laporan` FOREIGN KEY (`id_user`) REFERENCES `tb_user` (`id_user`);
+
+--
+-- Constraints for table `tb_materi`
+--
+ALTER TABLE `tb_materi`
+  ADD CONSTRAINT `fkid_kelas_materi` FOREIGN KEY (`id_kelas`) REFERENCES `tb_kelas` (`id_kelas`),
+  ADD CONSTRAINT `fkid_sub_materi_materi` FOREIGN KEY (`id_sub_materi`) REFERENCES `tb_sub_materi` (`id_sub_materi`);
+
+--
+-- Constraints for table `tb_progress_kelas`
+--
+ALTER TABLE `tb_progress_kelas`
+  ADD CONSTRAINT `fkid_kelas_pk` FOREIGN KEY (`id_kelas`) REFERENCES `tb_kelas` (`id_kelas`),
+  ADD CONSTRAINT `fkid_materi_pk` FOREIGN KEY (`id_materi`) REFERENCES `tb_materi` (`id_materi`),
+  ADD CONSTRAINT `fkid_user_pk` FOREIGN KEY (`id_user`) REFERENCES `tb_user` (`id_user`);
+
+--
+-- Constraints for table `tb_review`
+--
+ALTER TABLE `tb_review`
+  ADD CONSTRAINT `fkid_kelas_review` FOREIGN KEY (`id_kelas`) REFERENCES `tb_kelas` (`id_kelas`),
+  ADD CONSTRAINT `fkid_user_review` FOREIGN KEY (`id_user`) REFERENCES `tb_user` (`id_user`);
+
+--
+-- Constraints for table `tb_sub_materi`
+--
+ALTER TABLE `tb_sub_materi`
+  ADD CONSTRAINT `fkid_dokumen_sub_materi` FOREIGN KEY (`id_dokumen`) REFERENCES `tb_dokumen` (`id_dokumen`),
+  ADD CONSTRAINT `fkid_video_sub_materi` FOREIGN KEY (`id_video`) REFERENCES `tb_video` (`id_video`);
+
+--
+-- Constraints for table `tb_transaksi`
+--
+ALTER TABLE `tb_transaksi`
+  ADD CONSTRAINT `fkid_keranjang_transaksi` FOREIGN KEY (`id_keranjang`) REFERENCES `tb_keranjang` (`id_keranjang`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
